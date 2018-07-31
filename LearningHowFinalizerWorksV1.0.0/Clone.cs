0e017b2ddbb3117f6fa461187043e18fa4ba6af4
Hajk <tunczorx@gmail.com>
2018-07-31 08:53:16 +0200

    Since now method Dispose() let program serialize object before cleaning it.

--- LearningHowFinalizerWorksV1.0.0/Clone.cs
+++ LearningHowFinalizerWorksV1.0.0/Clone.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace LearningHowFinalizerWorksV1._0._0
 {
+    [Serializable]
     class Clone : IDisposable
     {
         public int Id { get; private set; }
@@ -18,9 +22,21 @@ namespace LearningHowFinalizerWorksV1._0._0
 
         public void Dispose()
         {
-            MessageBox.Show("Zostałem usunięty!", "Klon " + Id + ". mówi...");
-        }
+            string filename = @"C:\Temp\Klon.dat";
+            string dirname = @"C:\Temp\";
+            if(File.Exists(filename) == false)
+            {
+                Directory.CreateDirectory(dirname);
+            }
+            BinaryFormatter bf = new BinaryFormatter();
+            using (Stream output = File.OpenWrite(filename))
+            {
+                bf.Serialize(output, this);
+            }
 
+            MessageBox.Show("Tu " + this.Id + ", muszę .... zserializować... obiekt.");
+        }
+        
         ~Clone()
         {
             MessageBox.Show("Aaaaaaa! Dopadłeś mnie!", "Klon " + Id + ". mówi...");
